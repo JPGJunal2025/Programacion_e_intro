3c2346fb03a54d48004217321f500c99b49dd017
jgarzonji <jgarzonji@unal.edu.co>
2025-04-07 21:49:09 -0500

    Codigos_de_clase_intro

---
+++ 2025-04-04_Intro/Hola.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+    std::string nombre;
+    std::cout << "INGRESE SU NOMBRE: \n ";
+    std::cin >> nombre;
+    std::cout << "Hola, " << nombre << "\n" ;
+
+    return 0;
+}
