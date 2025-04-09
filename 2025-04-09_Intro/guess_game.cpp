7257ba3919fe505db430ac982d5509f79cb4c87b
jgarzonji <jgarzonji@unal.edu.co>
2025-04-09 08:34:47 -0500

    SE anhade la condicion de repeticion for

--- 2025-04-09_Intro/guess_game.cpp
+++ 2025-04-09_Intro/guess_game.cpp
@@ -3,16 +3,18 @@
 int main(void) {
     const int NUM = 10;
     int guessed_number;
-    std::cout << "Adivina un numero entre 1 y 100:\n";
-    std::cin >> guessed_number;
+    for ( ; ;) {
+        std::cout << "Adivina un numero entre 1 y 100:\n";
+        std::cin >> guessed_number;
 
-    if (guessed_number == NUM){
-        std::cout << "GANASTE!!!\n";
-    } else if (guessed_number > NUM){
-        std::cout << "Te pasaste\n";
-    } else {
-        std::cout << "Te falta\n";
+        if (guessed_number == NUM){
+            std::cout << "GANASTE!!!\n";
+            break;
+        } else if (guessed_number > NUM){
+            std::cout << "Te pasaste\n";
+        } else {
+            std::cout << "Te falta\n";
+        }
     }
-
     return 0;
 }
