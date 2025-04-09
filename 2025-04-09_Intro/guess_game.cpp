247770632081acd53317c99dcb9933d532c8a44c
jgarzonji <jgarzonji@unal.edu.co>
2025-04-09 07:39:05 -0500

    Primera version

---
+++ 2025-04-09_Intro/guess_game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void) {
+    const int NUM = 10;
+    int guessed_number;
+    std::cout << "Adivina un numero entre 1 y 100:\n";
+    std::cin >> guessed_number;
+
+    if (guessed_number == NUM){
+        std::cout << "GANASTE!!!\n";
+    }
+
+    return 0;
+}
