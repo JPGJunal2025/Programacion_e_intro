5c1d8bd198892634628f0e15e998c470d6ba943b
jgarzonji <jgarzonji@unal.edu.co>
2025-04-11 08:57:44 -0500

    Juego GOD

--- 2025-04-09_Intro/guess_game.cpp
+++ 2025-04-09_Intro/guess_game.cpp
@@ -1,20 +1,54 @@
 #include <iostream>
+#include <cstdlib>  // Para rand() y srand()
+#include <ctime>    // Para time()
 
-int main(void) {
-    const int NUM = 10;
-    int guessed_number;
-    for ( ; ;) {
-        std::cout << "Adivina un numero entre 1 y 100:\n";
+// Declaración
+void play(int min_val, int max_val);
+
+int main() {
+    // Inicializamos la semilla una sola vez
+    std::srand(std::time(0));
+
+    play(1, 100);
+    play(2, 22);
+
+    return 0;
+}
+
+// Implementación
+void play(int min_val, int max_val) {
+    const int MIN = min_val;
+    const int MAX = max_val;
+
+    // Número aleatorio entre MIN y MAX
+    const int NUM = MIN + (std::rand() % (MAX - MIN + 1));
+
+    int guessed_number = NUM / 2; // Solo valor inicial
+    int attempts = 0; // Contador de intentos
+
+    std::cout << "\nINICIA EL JUEGO\n";
+
+    while (guessed_number != NUM) {
+        std::cout << "Adivina un numero entre " << MIN << " y " << MAX << ":\n";
         std::cin >> guessed_number;
+        std::cout << "Escribiste: " << guessed_number << "\n";
 
-        if (guessed_number == NUM){
-            std::cout << "GANASTE!!!\n";
-            break;
-        } else if (guessed_number > NUM){
-            std::cout << "Te pasaste\n";
-        } else {
-            std::cout << "Te falta\n";
+        // Contamos el intento
+        attempts++;
+
+        // Validación del rango 
+        if (guessed_number < MIN || guessed_number > MAX) {
+            std::cout << "El numero no esta en el rango.\n";
+            continue;
+        }
+
+        if (guessed_number == NUM) {
+            std::cout << "GANASTE!!!! 🎉\n";
+            std::cout << "Lo lograste en " << attempts << " intento(s).\n";
+        } else if (guessed_number > NUM) {
+            std::cout << "Te pasaste.\n";
+        } else { // es menor
+            std::cout << "Te falta.\n";
         }
     }
-    return 0;
 }
