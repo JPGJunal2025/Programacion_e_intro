8dde707d2f21702e1722109ef21897c5a4bc517f
jgarzonji <jgarzonji@unal.edu.co>
2025-04-28 20:52:45 -0500

    2do codigo terminado

--- 2025-04-30_Intro/programa2.cpp
+++ 2025-04-30_Intro/programa2.cpp
@@ -1,44 +1,20 @@
 #include <iostream>
-#include <cmath>
 #include <string>
-
-//declaration
-
-bool verifyPrime(long n);
 
 int main(int argc, char **argv){
     long n = std::stol(argv[1]);
     long Max_Prime_value = 0;
+    long ii = 2;
 
-    for (long ii = 2; ii <= n; ++ii){
-        while (n % ii == 0) {
-        if (verifyPrime(ii)){
+    while (n > 1){
+        if (n % ii == 0){
             Max_Prime_value = ii;
-        }
-            break;
+            n /= ii;
+        } else {
+            ++ii;
         }
     }
+    
     std::cout << Max_Prime_value << "\n";
     return 0;
-}
-
-//implementation
-
-bool verifyPrime(long n){
-    if (n <= 1){
-        std::cout <<"Out of range number" << "\n";
-        return false;
-    }
-    bool flag = true;
-    if (n == 2){
-        return true;
-    }
-    for (long ii = 2; ii <= std::sqrt(n) + 1; ii++){
-        if (n % ii == 0){
-            flag = false;
-            break;
-        }
-    }
-
-    return flag;
 }
